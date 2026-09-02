da23dc940bad3cbd1963e6719e118d379c357bd7
Alphascleaning <ibrahimkamara161@gmail.com>
2026-09-02 14:40:00 +0200

    Fix Firebase configuration values

--- ja/firebase.js
+++ ja/firebase.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBTGRBvq-QUQcSsuFDadxJQeyB5E0_UME",
+  apiKey: "AIzaSyBTGRBvq-QQUcSsufDadxJQVeyB5E0_UME",
   authDomain: "klcrs-a7054.firebaseapp.com",
   projectId: "klcrs-a7054",
-  storageBucket: "klcrs-a7054.appspot.com",   // corrected & valid
+  storageBucket: "klcrs-a7054.appspot.com",
   messagingSenderId: "972485288867",
-  appId: "1:972485288867:web:8616aba476cf4d997fb016"
+  appId: "1:972485288867:web:8616aba476cfd4997fb016"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
