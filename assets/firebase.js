b6e1c1a3ac04ef0ac8e6fd54de797fe86877114e
Alphascleaning <ibrahimkamara161@gmail.com>
2026-09-03 11:44:07 +0200

    Add Firebase configuration and initialization

---
+++ assets/firebase.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBTGRBvq-QQUcSsufDadxJQVeyB5E0_UME",
+  authDomain: "klcrs-a7054.firebaseapp.com",
+  projectId: "klcrs-a7054",
+  storageBucket: "klcrs-a7054.appspot.com",
+  messagingSenderId: "972485288867",
+  appId: "1:972485288867:web:8616aba476cfd4997fb016"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Initialize Firestore
+window.firestore = firebase.firestore();
